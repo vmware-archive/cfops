22a9176ee9963082615727b25cd844b7488b9c6b
malston <mark_alston@yahoo.com>
2014-11-14 12:06:57 -0700

    Added some ginkgo test stubs

---
+++ backup/restore_test.go
@@ -0,0 +1,12 @@
+package backup_test
+
+import (
+	. "github.com/pivotalservices/cfops/backup"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Restore", func() {
+
+})
