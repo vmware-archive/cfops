4c61c44184164deb61ed7943e74eb6df1bf15bdb
Mark Alston <mark_alston@yahoo.com>
2014-12-04 16:44:40 -0600

    Removed irrelevant dependencies; fixed tests

--- system/command_runner_test.go
+++ system/command_runner_test.go
@@ -1,8 +1,8 @@
 package system_test
 
 import (
-	"github.com/malston/cf-logsearch-service-broker/system"
-	"github.com/pivotal-golang/lager/lagertest"
+	"github.com/cloudfoundry/gosteno"
+	"github.com/pivotalservices/cfops/system"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -12,7 +12,7 @@ var _ = Describe("A command runner", func() {
 	Context("is called with a valid command", func() {
 		It("should run successfully", func() {
 			commandRunner := &system.OSCommandRunner{
-				Logger: lagertest.NewTestLogger("command-runner-test"),
+				gosteno.NewLogger("TestLogger"),
 			}
 			err := commandRunner.Run("echo", "Hi", "there!")
 			Ω(err).ToNot(HaveOccurred())
@@ -21,7 +21,7 @@ var _ = Describe("A command runner", func() {
 	Context("is called with an invalid command", func() {
 		It("should fail with an error", func() {
 			commandRunner := &system.OSCommandRunner{
-				Logger: lagertest.NewTestLogger("command-runner-test"),
+				gosteno.NewLogger("TestLogger"),
 			}
 			err := commandRunner.Run("bad", "command")
 			Ω(err).To(HaveOccurred())
