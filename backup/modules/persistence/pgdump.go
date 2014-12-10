3b723615e9e31c7c639f9dbb8379ae17bed2a83d
Shaozhen Ding <dsz0111@gmail.com>
2014-12-10 17:41:35 -0500

    [#84015956]pgdump remote dump; password on command instead of os.Setenv;Fix port with getCommand() function

--- backup/modules/persistence/pgdump.go
+++ backup/modules/persistence/pgdump.go
@@ -3,7 +3,6 @@ package persistence
 import (
 	"fmt"
 	"io"
-	"os"
 
 	"github.com/pivotalservices/cfops/command"
 )
@@ -29,21 +28,26 @@ func NewPgDump(ip string, port int, database, username, password string) *PgDump
 	}
 }
 
-func (s *PgDump) Dump(dest io.Writer) (err error) {
-
-	if err = os.Setenv("PGPASSWORD", s.Password); err == nil {
-		err = s.executeDumpToWriter(dest)
-	}
-	return
+func NewPgRemoteDump(port int, database, username, password string, sshCfg command.SshConfig) (*PgDump, error) {
+	remoteExecuter, err := command.NewSshCopier(sshCfg)
+	return &PgDump{
+		Ip:       "localhost",
+		Port:     port,
+		Database: database,
+		Username: username,
+		Password: password,
+		Caller:   remoteExecuter,
+	}, err
 }
 
-func (s *PgDump) executeDumpToWriter(dest io.Writer) (err error) {
+func (s *PgDump) Dump(dest io.Writer) (err error) {
 	err = s.Caller.Execute(dest, s.getDumpCommand())
 	return
 }
 
 func (s *PgDump) getDumpCommand() string {
-	return fmt.Sprintf("pg_dump -h %s -U %s -p %s %s",
+	return fmt.Sprintf("PGPASSWORD=%s pg_dump -h %s -U %s -p %d %s",
+		s.Password,
 		s.Ip,
 		s.Username,
 		s.Port,
--- command/ssh.go
+++ command/ssh.go
@@ -2,13 +2,14 @@ package command
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 
 	"golang.org/x/crypto/ssh"
 )
 
 // Config for the SSH connection
-type Config struct {
+type SshConfig struct {
 	Username string
 	Password string
 	Host     string
@@ -34,6 +35,8 @@ type SshClientWrapper struct {
 	sshclient *ssh.Client
 }
 
+// Create copier based on client. Have direct ssh package reference
+
 func NewClientWrapper(client *ssh.Client) *SshClientWrapper {
 	return &SshClientWrapper{
 		sshclient: client,
@@ -44,10 +47,27 @@ func (c *SshClientWrapper) NewSession() (SSHSession, error) {
 	return c.sshclient.NewSession()
 }
 
+// Create copier based on client. Have direct ssh package reference
 func NewCopier(client ClientInterface) (copier *DefaultCopier) {
 	copier = &DefaultCopier{
 		client: client,
 	}
+	return
+}
+
+// This method creates copier based on ssh, it has concrete ssh reference
+func NewSshCopier(sshCfg SshConfig) (copier *DefaultCopier, err error) {
+	clientconfig := &ssh.ClientConfig{
+		User: sshCfg.Username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(sshCfg.Password),
+		},
+	}
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", sshCfg.Host, sshCfg.Port), clientconfig)
+	if err != nil {
+		return
+	}
+	copier = NewCopier(NewClientWrapper(client))
 	return
 }
 
