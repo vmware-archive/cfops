cfa408272ae97c2e48af7810b85d93dd3f62f11c
John Calabrese <jcalabrese@pivotal.io>
2015-07-20 16:46:20 -0400

    [#98674710] fixing flags and begging test cases
    we need some test cases to surface flag inconsistencies
    this is the beggining of that,
    
    we also fixed the flag missmatch to reflect the new
    opsmanager usernmae flag

---
+++ cmd/cfops/flags_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/codegangsta/cli"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("cli flags", func() {
+	Context("When defined in codegangsta", func() {
+		controlFields := map[string]bool{
+			strings.Join(opsManagerHostFlag, ", "): true,
+			strings.Join(directorUserFlag, ", "):   true,
+			strings.Join(directorPassFlag, ", "):   true,
+			strings.Join(opsManagerUserFlag, ", "): true,
+			strings.Join(opsManagerPassFlag, ", "): true,
+			strings.Join(destFlag, ", "):           true,
+			strings.Join(tilelistFlag, ", "):       true,
+		}
+
+		It("should have all registered fields", func() {
+
+			for _, v := range backupRestoreFlags {
+				flagName := v.(cli.StringFlag).Name
+				val, ok := controlFields[flagName]
+				Ω(val).Should(BeTrue())
+				Ω(ok).Should(BeTrue())
+			}
+		})
+
+		It("should contain the proper number of flags available", func() {
+			Ω(len(backupRestoreFlags)).Should(Equal(len(controlFields)))
+		})
+	})
+})
