b5e231b3b7d6003af3151fdd31ceb0c6234534ba
John Calabrese <jcalabrese@pivotal.io>
2015-11-17 16:59:41 -0500

    [#107948912] adding error to interface method New() should also return an error

--- tileregistry/types.go
+++ tileregistry/types.go
@@ -3,7 +3,7 @@ package tileregistry
 type (
 	//TileGenerator - interface for a tile creating object
 	TileGenerator interface {
-		New(tileSpec TileSpec) Tile
+		New(tileSpec TileSpec) (Tile, error)
 	}
 	//Tile - definition for what a tile looks like
 	Tile interface {
