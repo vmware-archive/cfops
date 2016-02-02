e382668a40b8a80bb8a756082cfb7f5e209a0bd8
John Calabrese <jcalabrese@pivotal.io>
2016-02-02 15:17:39 -0500

    [#112467509] better coverage for plugin calling

--- plugin/cfopsplugin/cfopsplugin_test.go
+++ plugin/cfopsplugin/cfopsplugin_test.go
@@ -1,24 +1,27 @@
 package cfopsplugin_test
 
 import (
+	"runtime"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/pivotalservices/cfops/plugin/cfopsplugin"
 )
 
 var _ = Describe("cfopsplugin", func() {
-	XDescribe("given a Call function", func() {
+	Describe("given a Call function", func() {
 		Context("when calling on a named plugin with path", func() {
-			It("", func() {
-				Call("", "")
-				Ω(true).Should(BeFalse())
+			It("should return a plugin which yield results from its methods", func() {
+				pl, cl := Call("backuprestore", "../load/fixture_plugins/"+runtime.GOOS+"/sample")
+				defer cl.Kill()
+				Ω(pl.Backup()).Should(HaveOccurred())
 			})
-		})
-	})
 
-	XDescribe("given a Start function", func() {
-		Context("when calling for meta data against a plugin", func() {
-
+			It("should return a plugin which yield results from its methods", func() {
+				pl, cl := Call("backuprestore", "../load/fixture_plugins/"+runtime.GOOS+"/sample")
+				defer cl.Kill()
+				Ω(pl.Restore()).ShouldNot(HaveOccurred())
+			})
 		})
 	})
 })
