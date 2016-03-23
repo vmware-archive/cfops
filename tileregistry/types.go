020725e248cb938edf2c27ab53b2f2707d73df61
John Calabrese <jcalabrese@pivotal.io>
2016-03-23 15:56:43 -0400

    [#116084083] adding tilespec element for clearboshmanifest toggle

--- tileregistry/types.go
+++ tileregistry/types.go
@@ -12,12 +12,13 @@ type (
 	}
 	//TileSpec -- defines what a tile would need to be initialized
 	TileSpec struct {
-		OpsManagerHost   string
-		AdminUser        string
-		AdminPass        string
-		OpsManagerUser   string
-		OpsManagerPass   string
-		ArchiveDirectory string
-		CryptKey         string
+		OpsManagerHost    string
+		AdminUser         string
+		AdminPass         string
+		OpsManagerUser    string
+		OpsManagerPass    string
+		ArchiveDirectory  string
+		CryptKey          string
+		ClearBoshManifest bool
 	}
 )
